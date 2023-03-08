28baf6f8643d68d92d6878fa6407cfe761557376
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2023-03-08 12:28:57 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,24 +21,3 @@ data "archive_file" "DataSource" {
     tags          = var.tags
   }
 
-resource "aws_lambda_permission" "allow_cloudwatch_to_call_lambda_Fun_StopEC2" {
-  count = var.enabled ? 1 : 0
-  statement_id  = "AllowExecutionFromCloudWatch"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.this.*.arn[0]
-  principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.CloudWatch_rule.*.arn[0]
-
-}
-
-resource "aws_cloudwatch_event_rule" "CloudWatch_rule" {
-  count = var.enabled ? 1 : 0
-  name        = var.function_name
-  description = var.description
-  schedule_expression = var.schedule_expression
-}
-resource "aws_cloudwatch_event_target" "lambda_schedular_target" {
-  count = var.enabled ? 1 : 0
-  rule = aws_cloudwatch_event_rule.CloudWatch_rule.*.name[0]
-  arn  = aws_lambda_function.this.*.arn[0]
-}
