e89e5d09fb80fd0d0dc834827e36744e3ab1afa7
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2023-03-09 12:29:32 -0800

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
-output "FunctionName" {
+output "function_arn" {
   value = aws_lambda_function.this.*.arn
+}
+output "function_name" {
+  description = "The name of the Lambda function"
+  value       = join("", aws_lambda_function.this.*.function_name)
 }
